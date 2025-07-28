0a6811e422365c501f2115f0a3fdae40e8bf9a7d
Harshit Shrivastav <87630299+itzharshit@users.noreply.github.com>
2025-07-28 15:50:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,10 @@
-import os
-import logging
-from http.server import HTTPServer, BaseHTTPRequestHandler
+from flask import Flask
+app = Flask(__name__)
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+@app.route('/')
+def hello_world():
+    return 'lol'
 
-class HealthHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        if self.path == "/kaithheathcheck":
-            self.send_response(200)
-            self.end_headers()
-            self.wfile.write(b"OK")
-        elif self.path == "/":
-            self.send_response(200)
-            self.end_headers()
-            self.wfile.write("Save-Contents bot is up :)".encode("utf-8"))
-        else:
-            self.send_response(404)
-            self.end_headers()
-
-def run_health_check_server(port):
-    try:
-        server = HTTPServer(("0.0.0.0", port), HealthHandler)
-        logging.info(f"[Health Check] Server starting on port {port}")
-        server.serve_forever()
-    except Exception as e:
-        logging.error(f"[Health Check] Failed to start server on port {port}: {e}")
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 8080))
-    run_health_check_server(port)
+    app.run()
